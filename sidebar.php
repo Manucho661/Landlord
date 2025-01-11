fa649f7177749f51d2a340afdde27e15d622d2be
BICCOUNT-TECHNOLOGIES <github@biccounttechnologies.com>
2025-01-11 11:12:55 +0300

    new incoming

--- sidebar.php
+++ sidebar.php
@@ -13,10 +13,10 @@
         <a href="index.php" onclick="toggleItems()">
         <i class="fas fa-tachometer-alt"></i> DASHBOARD </a><br>
         <a href="property.php"><i class="fas fa-building"></i> PROPERTY</a><br>
-        <a href="tenant.php"><i class="fas fa-users"></i> TENANTS</a><br>
-        <a href="rent.php"><i class="fas fa-receipt"></i> RENT</a><br>
+        <a href="news.php"><i class="fas fa-users"></i> NEWS</a><br>
         <a href="settings.php"> <i class="fas fa-cogs"></i> SETTINGS</a><br>
-        <a href="help.php"><i class="fas fa-question-circle"></i>HELP </a><br>
+        <a href="help.php"><i class="fas fa-question-circle"></i>HELP </a><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <a href="tenant.php"><i class="fas fa-sign-out-alt"></i> Logout</a><br>
 </div>
 </body>
 </html>
