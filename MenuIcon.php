8031895a811f93ec30f59078b250198719ac7a76
BICCOUNT-TECHNOLOGIES <github@biccounttechnologies.com>
2025-01-15 11:19:04 +0300

    new changes

--- MenuIcon.php
+++
@@ -1,9 +0,0 @@
-<div class="mb-4">
-
-<button id="toggleBtn" class="toggle-btn">
-          <span class="bar"></span>
-          <span class="bar"></span>
-          <span class="bar"></span>
-        </button>
-</div>
-
